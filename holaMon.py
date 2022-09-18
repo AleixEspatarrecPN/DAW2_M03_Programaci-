dab5c76a672ea71c7b1fbc589c12cfd7886af864
Aleix D. AllaBaix <94826231+AleixEspatarrecPN@users.noreply.github.com>
2022-09-18 18:36:56 +0200

    Add files via upload
    HolaMon i split per l'iniciació amb Python

---
+++ holaMon.py
@@ -0,0 +1,11 @@
+#Primer hola mon amb python, on descubrim la simplicitat d'aquest amb comparació a altres llenguatges com Java
+print ("hola mon")
+
+#Guardem el saludo en una variable
+saludo = "Hola mon, provem el split"
+
+#en aquesta nova variable utilitzem el metode split per dividir una cadena de carcters
+missatge = saludo.split()
+
+#finalment imprimim el resultat amb la funció print
+print(missatge)
